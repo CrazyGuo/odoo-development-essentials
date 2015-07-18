e32c9fa5ab135f921feb3b80fa36b82211dee615
johan <johanvergeer@gmail.com>
2015-07-18 12:33:30 +0200

    18-07-2015 12:32

---
+++ custom-addons/todo_app/__openerp__.py
@@ -0,0 +1,20 @@
+{
+    "name" : "To-Do Application",
+    "description" : "Manage your personal tasks with this module.",
+    "author" : "Johan Vergeer",
+    "depends" : ["mail"],
+    "application" : True,
+    "summary" : "You can manage your tasks with this module.",
+    "version" : "1.0",
+    "licence" : "AGPL-3",
+    "website" : "https://github.com/johanvergeer/Odoo-dev",
+    "category" : "I have to look for this"
+                 "category is the functional category of the module, which defaults to"
+                 "Uncategoryzed. This list of existing categories can be found in the security"
+                 "Groups form (settings | user | groups menu), in the Application field"
+                 "drop-down list.",
+    "installable" : True,
+    "auto_install" : True,
+    "data" : ["views/todo_view.xml"]
+
+}
