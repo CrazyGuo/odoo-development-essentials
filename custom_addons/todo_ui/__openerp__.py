1d7288800a5d4802d6178847761d8b995d2103e2
Johan Vergeer <johan.vergeer@b-informed.nl>
2015-07-25 09:26:53 +0200

    [ADD] todo_ui initial commit

---
+++ custom_addons/todo_ui/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{'name': 'User interface improvements to the To-Do app',
+ 'version': '1.0',
+ 'author': 'Johan Vergeer',
+ 'maintainer': 'Johan Vergeer',
+ 'depends': ['todo_app'],
+ 'description': """
+User interface improvements to the To-Do app
+
+ """,
+ 'website': '',
+ 'data': [''],
+ 'installable': True,
+ 'images': [],
+ 'license': 'AGPL-3',
+ }
