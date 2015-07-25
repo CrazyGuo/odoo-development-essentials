e6aed9bc8d1cdfd0d91907292a28cc09012d9e63
Johan Vergeer <johan.vergeer@b-informed.nl>
2015-07-25 15:04:53 +0200

    [ADD] Created new views for the To-Do app

--- custom_addons/todo_ui/__openerp__.py
+++ custom_addons/todo_ui/__openerp__.py
@@ -9,6 +9,7 @@
 User interface improvements to the To-Do app
 This module depends on todo_app and todo_user
  """,
+ 'data' : ["views/todo_view.xml"],
  'website': 'https://github.com/johanvergeer/Odoo-dev/tree/master/custom_addons/todo_ui',
  'installable': True,
  'license': 'AGPL-3',
