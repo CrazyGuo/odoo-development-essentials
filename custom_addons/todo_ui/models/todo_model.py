d575243e17b139d08dbbf1310b68808446c84b28
Johan Vergeer <johan.vergeer@b-informed.nl>
2015-07-26 12:21:31 +0200

    [IMP] End of chapter 6

--- custom_addons/todo_ui/models/todo_model.py
+++ custom_addons/todo_ui/models/todo_model.py
@@ -1,10 +1,10 @@
 # -*- codingL utf-8 -*-
+from django.utils import dateformat
 
 from openerp import models, fields, api
 from openerp.addons.base.res import res_request
 from openerp.exceptions import ValidationError
-import datetime
-
+from datetime import date, datetime, time
 
 def referencable_models(self):
     return res_request.referencable_models(self, self.env.cr, self.env.uid, context=self.env.context)
@@ -19,6 +19,7 @@ class Tag(models.Model):
     parent_left = fields.Integer("Parent left", index=True)
     parent_right = fields.Integer("Parent Right", index=True)
     child_ids = fields.One2many("todo.task.tag", "parent_id", "Child Tags")
+    _date_deadline_passed = False
 
 
 class Stage(models.Model):
@@ -44,6 +45,7 @@ class Stage(models.Model):
 
 class TodoTask(models.Model):
     _inherit = "todo.task"
+
     stage_id = fields.Many2one("todo.task.stage", "Stage")
     tag_ids = fields.Many2many("todo.task.tag", string="Tags")
     refers_to = fields.Reference(referencable_models, string="Refers to")
@@ -51,6 +53,8 @@ class TodoTask(models.Model):
                                 inverse="_inverse_stage_fold")
     stage_state = fields.Selection(related="stage_id.state", string="Stage State")
     _sql_constraints = [("todo_task_name_uniq", "UNIQUE (name, user_id, active)", "Task title must be unique!")]
+    date_start = fields.Date("Start Date")
+
 
     @api.one
     @api.depends
@@ -70,6 +74,13 @@ class TodoTask(models.Model):
         if len(self.name) < 5:
             raise ValidationError("Must have 5 chars!")
 
+    # check if the start date is before the deadline
+    @api.one
+    @api.constrains("date_deadline","date_start")
+    def _date_start_before_date_deadline(self):
+        if self.date_start > self.date_deadline:
+            raise ValidationError("The start date must be before the deadline!")
+
     # compute the amount of tasks for the user
     @api.one
     def compute_user_todo_count(self):
@@ -77,9 +88,16 @@ class TodoTask(models.Model):
 
     user_todo_count = fields.Integer("User To-Do Count", compute="compute_user_todo_count")
 
-    @api.one
-    def date_deadline_passed(self):
-        if not self.date_deadline > datetime.date.today():
-            return True
-        else:
-            return False
+    # compute the days needed to complete a task
+    # @api.one
+    # def compute_effort_estimate(self):
+    #     startdate = datetime.strptime(self.date_start., "%d")
+    #     deadlinedate = datetime.strptime("self.date_deadline", "%d")
+    #
+    #
+    #     self.effort_estimate = (deadlinedate - startdate).days
+    #
+    # effort_estimate = fields.Date("Effort Estimate (Days)", compute="compute_effort_estimate")
+
+    # TODO effort_estimate automatisch laten berekenen als date_start en date_deadline zijn ingevuld.
+    effort_estimate = fields.Integer("Effort estimate (Days)")
