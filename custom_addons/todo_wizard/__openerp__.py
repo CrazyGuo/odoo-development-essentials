0441a653a89d17a6d0c32c0ad49a1e62adcd301e
Johan Vergeer <johan.vergeer@b-informed.nl>
2015-07-26 15:14:05 +0200

    [ADD] Initial commit Todo Wizard

---
+++ custom_addons/todo_wizard/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#
+
+{'name': 'To-Do Tasks Management Assistant',
+ 'version': '1.0',
+ 'author': 'Johan Vergeer',
+ 'maintainer': 'Johan Vergeer',
+ 'depends': ['todo_user'],
+ 'description': """
+To-Do Task Management Assistand
+ """,
+ 'website': 'http://www.b-informed.nl',
+ 'data': [''],
+ 'installable': True,
+ 'images': [],
+ 'license': 'AGPL-3',
+ }
